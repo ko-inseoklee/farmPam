e5d92673f84a485e0fe4f7d066cb30ff3584d80a
YechanLim <56158485+YechanLim@users.noreply.github.com>
2020-11-29 02:31:23 +0900

    Create farmPage.dart

---
+++ lib/farmPage.dart
@@ -0,0 +1,75 @@
+import 'assets.dart';
+import 'package:flutter/material.dart';
+import 'signIn.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class farmPage extends StatefulWidget {
+  @override
+  _farmPageState createState() => _farmPageState();
+}
+
+class _farmPageState extends State<farmPage> {
+  //Todo: title will be friend name.
+  String title = "Farm";
+  var imageReference;
+
+  String farmName_;
+  String farmImage_;
+  String farmDescription_;
+  String farmLocation_;
+  List<dynamic> farmReview_;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+        stream: currentUser.snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> document) {
+          if (document.hasError) {
+            print("something wrong");
+          }
+          if (document.connectionState == ConnectionState.waiting) {
+            print("wait..");
+          }
+          farmName_ = document.data['farmName'];
+          farmImage_ = document.data['farmImage'];
+          farmDescription_ = document.data['farmDescription'];
+          imageReference = document.data['image'];
+        });
+
+    return Scaffold(
+      appBar: header(context, title),
+      body: ListView(
+        children: <Widget>[
+          FutureBuilder(
+              future: imageReference.getDownloadURL(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return CircularProgressIndicator();
+                return Image.network(snapshot.data);
+              }),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: <Widget>[
+              Text(farmName_),
+              SizedBox(
+                width: 20,
+              ),
+              Text(farmLocation_),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 1,
+            color: Colors.grey,
+          ),
+          Text(farmDescription_),
+        ],
+      ),
+      bottomNavigationBar: footer(context),
+    );
+  }
+}
