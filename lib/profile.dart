084e53877e4723ff5a10f88b18312b4852dcf239
YechanLim <56158485+YechanLim@users.noreply.github.com>
2020-11-29 02:30:00 +0900

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,8 @@
-import 'package:farmpam/assets.dart';
+import 'assets.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'signIn.dart';
+import 'farmPage.dart';
 
 class profilePage extends StatefulWidget {
   @override
@@ -8,11 +11,82 @@ class profilePage extends StatefulWidget {
 
 class _profilePageState extends State<profilePage> {
   String title = "Profile";
-
+  String address_;
+  String nickName_;
+  List<dynamic> sellingProducts_;
+  var imageReference;
   @override
   Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+        stream: currentUser.snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> document) {
+          if (document.hasError) {
+            print("something wrong");
+          }
+          if (document.connectionState == ConnectionState.waiting) {
+            print("wait..");
+          }
+          address_ = document.data['address'];
+          nickName_ = document.data['nickName'];
+          sellingProducts_ = document.data['sellingProducts'];
+          imageReference = document.data['image'];
+        });
+
     return Scaffold(
       appBar: header(context, title),
+      body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+        Row(
+          children: <Widget>[
+            //TODO: 왼쪽 위에 이미지 나오게
+            FutureBuilder(
+                future: imageReference.getDownloadURL(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return CircularProgressIndicator();
+                  return Image.network(snapshot.data);
+                }),
+            /* Container(
+                height: 70,
+                width: 50,
+                color: Colors.red,
+                child: Text(
+                  'picture',
+                )),*/
+            //TODO: image옆에 닉네임, 장소
+            Container(
+                height: 70,
+                width: 150,
+                child: Column(
+                  children: <Widget>[
+                    Text(nickName_),
+                    Container(
+                      height: 1,
+                      width: 50,
+                      color: Colors.grey,
+                    ),
+                    Text(address_),
+                  ],
+                ))
+          ],
+        ),
+        Container(
+          height: 1,
+          color: Colors.grey,
+        ),
+        //TODO: FarmPage로 가는 버튼과 구매한 목록 이동 버튼 구현
+        Row(
+          children: <Widget>[
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => farmPage()));
+                },
+                child: Text('goToFarmPage')),
+            TextButton(onPressed: null, child: Text('goToBoughtProductsPage')),
+          ],
+        ),
+        //TODO: 판매중인 목록
+      ]),
       bottomNavigationBar: footer(context),
     );
   }
